f12221e3e5a843adb5a8099bb59f5344bd7c393b
foqc <fabianq44@gmail.com>
2023-05-22 20:22:06 -0500

    feat(GC-005): fetching user artitcles fro Dev.to's api

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,5 +1,44 @@
-export type Article = {
-  [key: string]: object;
+export type DevArticleType = {
+  type_of: string;
+  id: number;
+  title: string;
+  description: string;
+  cover_image: string;
+  readable_publish_date: string;
+  social_image: string;
+  tag_list: string[];
+  tags: string;
+  slug: string;
+  path: string;
+  url: string;
+  canonical_url: string;
+  comments_count: number;
+  positive_reactions_count: number;
+  public_reactions_count: number;
+  collection_id: number;
+  created_at: string;
+  edited_at: string | null;
+  crossposted_at: string | null;
+  published_at: string;
+  last_comment_at: string;
+  published_timestamp: string;
+  reading_time_minutes: number;
+  user: {
+    name: string;
+    username: string;
+    twitter_username: string;
+    github_username: string;
+    website_url: string;
+    profile_image: string;
+    profile_image_90: string;
+  };
+  organization: {
+    name: string;
+    username: string;
+    slug: string;
+    profile_image: string;
+    profile_image_90: string;
+  };
 };
 export const sourceNodes = async ({ actions, createNodeId, createContentDigest }, { username }) => {
   const { createNode } = actions;
@@ -14,12 +53,12 @@ export const sourceNodes = async ({ actions, createNodeId, createContentDigest }
 
   const response = await fetch(`https://dev.to/api/articles?username=${username}`);
   const data = await response.json();
-  const articlesPromise: Promise<Article>[] = data.map(({ id }) =>
+  const articlesPromise: Promise<DevArticleType>[] = data.map(({ id }) =>
     fetch(`https://dev.to/api/articles/${id}`).then((result) => result.json())
   );
 
-  const articles: Article[] = await Promise.all(articlesPromise);
-  articles.forEach((article: Article) => {
+  const articles: DevArticleType[] = await Promise.all(articlesPromise);
+  articles.forEach((article: DevArticleType) => {
     const gatsbyNode = {
       article,
       id: createNodeId(`${article.id}`),
